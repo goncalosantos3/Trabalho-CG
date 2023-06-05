28a77d2755dc45a25c574a77e37057ec9844abf4
JoaoNovais181 <a96626@alunos.uminho.pt>
2023-06-05 23:37:57 +0100

    removi coisas

--- prepareDemoScene.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-cd build
-make
-./generator sphere 1 52 52 sphere.3d
-./generator sphereInv 1 52 52 stars.3d
-./generator washer 1 1.2 0.0005 32 saturnRing.3d
-./generator bezier teapot.patch 10 teapot.3d
-./engine SolarSystem.xml
